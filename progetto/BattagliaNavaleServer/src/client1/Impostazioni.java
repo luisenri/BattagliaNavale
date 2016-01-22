7d7f68fac360e1ae0afdc5962673316d19416108
luisenri <luisenri86@gmail.com>
2016-01-23 00:12:55 +0100

    aggiornamento impostazioni

--- progetto/BattagliaNavaleServer/src/client1/Impostazioni.java
+++ progetto/BattagliaNavaleServer/src/client1/Impostazioni.java
@@ -2,6 +2,7 @@ package client1;
 
 public class Impostazioni {
 	public static final int PORTA_SERVER = 2000;
+	//public static final String URL_SERVER = "rmi://192.168.1.6:"+PORTA_SERVER+"/server";
 	public static final String URL_SERVER = "rmi://127.0.0.1:"+PORTA_SERVER+"/server";
 	public static final String PATH_POLICY = "file:/C:/Users/PERS/workspacePersonale/BattagliaNavaleServer/src/client1/server.policy";
 	//public static final String PATH_POLICY = "file:/C:/Users/Vaio/Documents/workspace/BattagliaNavaleServer/src/client1/server.policy";
